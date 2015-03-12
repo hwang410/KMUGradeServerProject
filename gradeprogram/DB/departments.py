ecd0dcc761e89d26a59b5b8fc7d744d66710d292
dollking <algorithm@localhost.localdomain>
2015-03-12 19:55:57 +0900

    renewal

---
+++ gradeprogram/DB/departments.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    photolog.model.photo
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    photolog 어플리케이션을 사용할 사용자 정보에 대한 model 모듈.
+
+    :copyright: (c) 2013 by 4mba.
+    :license: MIT LICENSE 2.0, see license for more details.
+"""
+
+
+from sqlalchemy import Column
+from sqlalchemy.dialects.mysql import VARCHAR, INTEGER, ENUM
+
+from DB import Base
+
+class Departments(Base) :
+    
+    __tablename__ ='Departments'
+    
+    departmentIndex =Column (INTEGER (unsigned =True), primary_key =True, autoincrement =True, nullable =False)
+    departmentCode =Column (VARCHAR (128), nullable =False)
+    departmentName =Column (VARCHAR (1024), nullable =False)
+    isAbolished =Column (ENUM ('Abolished', 'Not-Abolished'), default ='Not-Abolished', nullable =False)
+    
