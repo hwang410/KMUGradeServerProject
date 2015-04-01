07a0107cfeb5d60fa31c1b44591da0df0edc5f18
jongseok <jseok.lee2@gmail.com>
2015-04-01 18:22:11 +0900

    from celeryServer .. add

--- pydev/GradeServer/GradeServer/GradeServer/controller/codesubmission.py
+++ pydev/GradeServer/GradeServer/GradeServer/controller/codesubmission.py
@@ -26,8 +26,7 @@ from GradeServer.model.languagesOfCourses import LanguagesOfCourses
 from GradeServer.model.departmentsDetailsOfMembers import DepartmentsDetailsOfMembers
 from GradeServer.GradeServer_config import GradeServerConfig
 from sqlalchemy import and_, func
-from datetime import datetime
-#from GradeServer.celeryServer import Grade
+from celeryServer_existingcontainer import Grade
 
 # Initialize the Flask application
 ALLOWED_EXTENSIONS = set(['py', 'java', 'class', 'c', 'cpp', 'h', 'jar'])
