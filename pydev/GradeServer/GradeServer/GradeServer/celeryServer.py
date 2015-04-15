dfe4747053d4d1b88354e8f420e11bfd8c983a35
dollking <dollking2000@naver.com>
2015-04-15 19:41:02 +0900

    code style is modifed to google code style.

--- pydev/GradeServer/GradeServer/GradeServer/celeryServer.py
+++ pydev/GradeServer/GradeServer/GradeServer/celeryServer.py
@@ -7,20 +7,24 @@ MAX_CONTAINER_COUNT = 10
 app = Celery('tasks', broker = 'redis://192.168.0.119:6379')
 
 @app.task(name = 'task.Grade')
-def Grade(filePath, problemPath, stdNum, problemNum, gradeMethod, caseCount, limitTime, limitMemory, usingLang, version, courseNum, submitCount):
+def Grade(filePath, problemPath, stdNum, problemNum, gradeMethod, caseCount,
+          limitTime, limitMemory, usingLang, version, courseNum, submitCount):
     argsForm = '%s %s %s %s %s %i %i %i %s %s %s %i'
-    argsList = argsForm % (filePath, problemPath, stdNum, problemNum, gradeMethod, caseCount, limitTime, limitMemory,\
-                           usingLang, version, courseNum, submitCount)
+    argsList = argsForm % (filePath, problemPath, stdNum, problemNum, gradeMethod,
+                           caseCount, limitTime, limitMemory, usingLang, version,
+                           courseNum, submitCount)
 
     worker_num = current_process().index + 1
 
-    os.system('sudo docker exec grade_container' + str(worker_num) + ' python /gradeprogram/rungrade.py ' + argsList) 
+    os.system('sudo docker exec grade_container' + str(worker_num) +
+              ' python /gradeprogram/rungrade.py ' + argsList) 
     
 @app.task(name = 'task.ServerOn')
 def ServerOn():
     for i in range(MAX_CONTAINER_COUNT):
         number = str(i+1)
-        os.system('sudo docker create --privileged -i -t --name grade_container' + number + ' gradeserver:1.0 /bin/bash')
+        os.system('sudo docker create --privileged -i -t --name grade_container'
+                  + number + ' gradeserver:1.0 /bin/bash')
         os.system('sudo docker start grade_container' + number)
     
 @app.task(name = 'task.ServerOff')
