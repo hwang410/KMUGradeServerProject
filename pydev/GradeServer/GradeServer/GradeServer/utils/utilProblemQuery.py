c59f00f0d809ba83d2d5823106cf5566e5b7e5c5
astroluj <astrolujx@naver.com>
2015-05-06 16:48:15 +0900

    warning clear

--- pydev/GradeServer/GradeServer/GradeServer/utils/utilProblemQuery.py
+++ pydev/GradeServer/GradeServer/GradeServer/utils/utilProblemQuery.py
@@ -1,27 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import and_, func
 
 from datetime import datetime
 
-from GradeServer.resource.enumResources import ENUMResources
-from GradeServer.resource.setResources import SETResources
-from GradeServer.resource.htmlResources import HTMLResources
-from GradeServer.resource.routeResources import RouteResources
-from GradeServer.resource.otherResources import OtherResources
-from GradeServer.resource.sessionResources import SessionResources
-
 from GradeServer.database import dao
-from GradeServer.GradeServer_blueprint import GradeServer
 
 from GradeServer.model.registeredProblems import RegisteredProblems
-from GradeServer.model.registeredCourses import RegisteredCourses
-from GradeServer.model.problems import Problems
 from GradeServer.model.submissions import Submissions
-from GradeServer.model.submittedFiles import SubmittedFiles
-from GradeServer.model.submittedRecordsOfProblems import SubmittedRecordsOfProblems
-from GradeServer.model.languages import Languages
-from GradeServer.model.languagesOfCourses import LanguagesOfCourses
 from GradeServer.model.problems import Problems
 
 
