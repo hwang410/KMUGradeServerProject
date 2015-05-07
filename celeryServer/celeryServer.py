8f6257c877b45d63d4c8652ccf78855afb52293a
dollking <dollking2000@naver.com>
2015-05-07 14:14:35 +0900

    database update command is split by function.

--- celeryServer/celeryServer.py
+++ celeryServer/celeryServer.py
@@ -1,6 +1,5 @@
 import os
 import time
-import DBUpdate
 from celery import Celery
 from subprocess import Popen, PIPE
 from dbinit import DatabaseInit
@@ -26,15 +25,45 @@ def Grade(filePath, problemPath, stdNum, problemNum, gradeMethod, caseCount,
                                          worker_num,
                                          ' python /gradeprogram/rungrade.py ')
 
-    message = Popen(containerCreadeCommand + argsList, shell=True, stdout=PIPE)
+    print 'program start'
+    message = Popen(containerCreadeCommand + argsList, shell=True, stderr=PIPE)
     
     messageLines = message.stdout.readlines()
-    messageLine = messageLines[-1]
     
-    result = messageLine[0]
-    score = messageLine[1]
-    runTime = messageLine[2]
-    usingMem = messageLine[3]
+    resultUpdate(messageLine = messageLines[-1], stdNum, problemNum, courseNum, submitCount)
+    
+@app.task(name = 'task.ServerOn')
+def ServerOn():
+    for i in range(MAX_CONTAINER_COUNT):
+        containerNum = i + 1
+        containerCreadeCommand = "%s%i%s%i%s" %('sudo docker create --privileged -i -t --name --cpuset="',
+                                                i, '" grade_container', containerNum,
+                                                ' gradeserver:1.0 /bin/bash')
+        
+        runProgramInContainer = '%s%i%s' % ('sudo docker exec grade_container',
+                                            containerNum, 'python -B /gradeprogram/*')
+        os.system(containerCreadeCommand)
+        os.system('sudo docker start grade_container' + containerNum)
+        os.system(runProgramInContainer)
+    
+@app.task(name = 'task.ServerOff')
+def ServerOff():
+    time.sleep(5)
+    
+    for i in range(MAX_CONTAINER_COUNT):
+        number = str(i+1)
+        os.system('sudo docker stop grade_container' + number)
+        os.system('sudo docker rm grade_container' + number)
+        
+def resultUpdate(messageLine, stdNum, problemNum, courseNum, submitCount):
+    import DBUpdate
+    
+    messageParaList = messageLine.split() 
+    
+    result = messageParaList[0]
+    score = messageParaList[1]
+    runTime = messageParaList[2]
+    usingMem = messageParaList[3]
     
     dataUpdate = DBUpdate.DBUpdate(stdNum, problemNum, courseNum, submitCount)
     
@@ -62,26 +91,3 @@ def Grade(filePath, problemPath, stdNum, problemNum, gradeMethod, caseCount,
     else:
         dataUpdate.UpdateServerError(stdNum, problemNum,
                                    courseNum, submitCount)
-    
-@app.task(name = 'task.ServerOn')
-def ServerOn():
-    for i in range(MAX_CONTAINER_COUNT):
-        containerNum = i + 1
-        containerCreadeCommand = "%s%i%s%i%s" %('sudo docker create --privileged -i -t --name --cpuset="',
-                                                i, '" grade_container', containerNum,
-                                                ' gradeserver:1.0 /bin/bash')
-        
-        runProgramInContainer = '%s%i%s' % ('sudo docker exec grade_container',
-                                            containerNum, 'python -B /gradeprogram/*')
-        os.system(containerCreadeCommand)
-        os.system('sudo docker start grade_container' + containerNum)
-        os.system(runProgramInContainer)
-    
-@app.task(name = 'task.ServerOff')
-def ServerOff():
-    time.sleep(5)
-    
-    for i in range(MAX_CONTAINER_COUNT):
-        number = str(i+1)
-        os.system('sudo docker stop grade_container' + number)
-        os.system('sudo docker rm grade_container' + number)
