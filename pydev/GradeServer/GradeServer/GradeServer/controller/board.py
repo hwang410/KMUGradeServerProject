072f1a789132464aff38208f53cccb49a18272c5
astroluj <astrolujx@naver.com>
2015-05-13 14:40:24 +0900

    board view fix

--- pydev/GradeServer/GradeServer/GradeServer/controller/board.py
+++ pydev/GradeServer/GradeServer/GradeServer/controller/board.py
@@ -78,7 +78,8 @@ def board(activeTabCourseId, pageNum):
                                              myCourses).subquery()
                 # 과목 공지글    
         try:  
-            articleNoticeRecords = get_page_record((select_sorted_articles(activeTabCourseId = articlesOnBoard,
+            print len(dao.query(articlesOnBoard).all())
+            articleNoticeRecords = get_page_record((select_sorted_articles(articlesOnBoard,
                                                                            isNotice = ENUMResources().const.TRUE)),
                                                    pageNum = int(1),
                                                    LIST = OtherResources().const.NOTICE_LIST).all()
