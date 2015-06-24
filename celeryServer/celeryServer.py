dd4ae4bd23561ae3a8a720d76864a524c5b8ff44
황석진 <dollking@users.noreply.github.com>
2015-06-24 17:37:20 +0900

    Update celeryServer.py

--- celeryServer/celeryServer.py
+++ celeryServer/celeryServer.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 from celery import Celery
 
 app = Celery('tasks', broker = 'redis://192.168.0.8:6379', include=['tasks'])
-
+#test
 if __name__ == '__main__':
     app.start()
